dd4d476478cf67b974dc7effd0a729aeae3e074c
NegreanV <yo_victor_00@yahoo.com>
2015-10-20 19:47:41 +0300

    Create Problema1.cpp

---
+++ 20octombrie/Problema1.cpp
@@ -0,0 +1,22 @@
+//Se citeste un numar natural n cu cel mult 4 cifre. 
+//Sa se afiseze in ordine crescatoare toate numele naturale pare atat timp cat suma lor nu depaseste pe n.
+//Ex: pentru n=15 se afiseaza 2 4 6 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, used;
+	cin >> a;
+	
+	for (int i = 2; i <= a; i++)
+	{
+		if (i % 2 == 0)
+		{
+			cout << i << endl;
+			used += i;
+		}
+	}
+}
