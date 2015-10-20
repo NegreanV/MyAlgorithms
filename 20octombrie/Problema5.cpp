c17c29c3079f6c80a1a84a4891343428b4c5bdc6
NegreanV <yo_victor_00@yahoo.com>
2015-10-20 19:50:22 +0300

    Create Problema5.cpp

---
+++ 20octombrie/Problema5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, sumaDiv, sumaDiviz = 0, keep = 112;
+	
+	cout << "n = ";
+	cin >> n;
+	
+	int numere[n];
+	
+	for (int i = 0; i < n; i++)
+	{
+		cin >> numere[i];
+	}
+	
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 1; j <= 50; j++)
+		{
+			if (numere[i] % j == 0)
+			{
+				sumaDiv += j;
+			}
+		}
+		if (sumaDiv > sumaDiviz)
+		{
+			sumaDiviz = sumaDiv;
+			keep = numere[i];
+		}
+	}
+	cout << keep;
+}
